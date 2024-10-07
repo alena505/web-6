04b527ed7b82cd87069dceb05bef412f6cce9b82
Алена Авдеева <alenaavdeeva@MacBook-Pro-Alena.local>
2024-10-07 23:55:28 +0300

    main.go2

--- projects/2_query/main.go
+++ projects/2_query/main.go
@@ -1,9 +1,18 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
+	"net/http"
+)
 
-func main() {
-	// и здесь тоже
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello," + r.URL.Query().Get("name") + "!"))
 }
 
-// и тут тоже (если очень надо)
+func main() {
+	http.HandleFunc("/api/user", handler)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
